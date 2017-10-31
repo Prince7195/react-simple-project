7cbbbbf52c43305793b16e3f1907d098635eb406
Vijay Deepak <vijaydeepak7195@outlook.com>
2017-10-31 14:34:43 +0530

    updated till adding simple contact

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
 
+import data from './data';
+import Contact from './Contact';
+
 class App extends React.Component {
+
+  componentWillMount() {
+    this.setState({
+        Contacts: data
+    });
+  }  
+
+  addContact = (e) => {
+    e.preventDefault();
+    let contacts = this.state.Contacts;
+    let newId = contacts[contacts.length -1].id + 1;
+    let newContact = {
+      id: newId,
+      name: `New Contact ${newId}`,
+      email: `${newId}@example.com`
+    }
+    contacts.push(newContact);
+    this.setState({ Contacts: contacts });
+  }
+
   render() {
     return (
-      <div className="App">
-        Welcome to React Hell!
-        <hr />
-      </div>
+      <div id="App">
+        <a href="#" className="pure-button" onClick={this.addContact}>Add Contact</a>
+        <div className="pure-g">
+          {/* {data.map(
+            info => <Contact name={info.name} email={info.email}/> 
+          )} */} 
+          { 
+            // above and below gives the same output 
+          }
+          {this.state.Contacts.map(
+            info => <Contact key={info.id} {...info}/> 
+          )}
+        
+        </div>
+      </div>      
     );
   }
 }
